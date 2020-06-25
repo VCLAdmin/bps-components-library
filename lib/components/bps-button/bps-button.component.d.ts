43079ef1bfdefab4e2aed13da80c2b83afdbc9ea
Malini Yarla <malini.yarla@sidhiconsulting.com>
2020-06-25 12:47:24 -0400

    Right Panel + Editable Button

--- lib/components/bps-button/bps-button.component.d.ts
+++ lib/components/bps-button/bps-button.component.d.ts
@@ -1,7 +1,7 @@
 import { ContentObserver } from '@angular/cdk/observers';
-import { AfterContentInit, ChangeDetectorRef, ElementRef, NgZone, OnChanges, OnDestroy, OnInit, QueryList, Renderer2, SimpleChanges } from '@angular/core';
+import { AfterContentInit, ChangeDetectorRef, ElementRef, NgZone, OnChanges, OnDestroy, OnInit, QueryList, Renderer2, SimpleChanges, EventEmitter } from '@angular/core';
 import { NzConfigService, NzSizeLDSType, NzUpdateHostClassService, NzWaveConfig, NzWaveDirective } from 'ng-zorro-antd/core';
-export declare type NzButtonType = 'primary' | 'dashed' | 'danger' | 'default' | 'link' | 'variation-1' | 'variation-2' | 'variation-3' | 'variation-4' | 'variation-5' | 'variation-6' | 'variation-7' | 'variation-8' | 'variation-9' | 'variation-10' | 'variation-11' | 'variation-12' | 'variation-13' | 'variation-14' | 'variation-15' | 'variation-16' | 'variation-17' | 'variation-18' | 'variation-19' | 'variation-20';
+export declare type NzButtonType = 'primary' | 'dashed' | 'danger' | 'default' | 'link' | 'variation-1' | 'variation-2' | 'variation-3' | 'variation-4' | 'variation-5' | 'variation-6' | 'variation-7' | 'variation-8' | 'variation-9' | 'variation-10' | 'variation-11' | 'variation-12' | 'variation-13' | 'variation-14' | 'variation-15' | 'variation-16' | 'variation-17' | 'variation-18' | 'variation-19' | 'variation-20' | 'editable';
 export declare type NzButtonShape = 'circle' | 'round' | null;
 export declare class BpsButtonComponent implements AfterContentInit, OnInit, OnDestroy, OnChanges {
     private elementRef;
@@ -13,16 +13,22 @@ export declare class BpsButtonComponent implements AfterContentInit, OnInit, OnD
     nzConfigService: NzConfigService;
     private waveConfig;
     private animationType;
+    isSelected: boolean;
+    showEditionMode: boolean;
     contentElement: ElementRef;
     listOfIconElement: QueryList<ElementRef>;
     nzWave: NzWaveDirective;
     bpsBlock: boolean;
     bpsGhost: boolean;
     bpsSearch: boolean;
+    bpsComputed: boolean;
     bpsLoading: boolean;
     bpsType: NzButtonType;
+    bpsValue: string;
+    bpsValueChange: EventEmitter<any>;
     bpsShape: NzButtonShape;
     bpsSize: NzSizeLDSType;
+    inputElement: ElementRef;
     readonly el: HTMLElement;
     isInDropdown: boolean;
     private iconElement;
@@ -33,6 +39,12 @@ export declare class BpsButtonComponent implements AfterContentInit, OnInit, OnD
     updateIconDisplay(value: boolean): void;
     checkContent(): void;
     moveIcon(): void;
+    preventDefault($event: any): void;
+    handleClick(e: MouseEvent): void;
+    endEditMode($event: KeyboardEvent): void;
+    clicks: number;
+    onClick(event: MouseEvent): void;
+    onDblClick(event: MouseEvent): void;
     constructor(elementRef: ElementRef, cdr: ChangeDetectorRef, renderer: Renderer2, contentObserver: ContentObserver, nzUpdateHostClassService: NzUpdateHostClassService, ngZone: NgZone, nzConfigService: NzConfigService, waveConfig: NzWaveConfig, animationType: string);
     ngAfterContentInit(): void;
     ngOnInit(): void;
